79c4ef318a68431290bae08bf029aa174ca0bc8d
Swag <swagato@js-MacBook-Pro.local>
2017-11-07 19:02:34 +0800

    WIP: train.py

--- train.py
+++ train.py
@@ -1,3 +1,18 @@
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import matplotlib
+
+import os
+
+# data files available for training
+# train.csv, store.csv
+
+# locations of data files
+root = os.path.dirname(os.path.realpath(__file__))
+storeFile = os.path.join(root, "store.csv")
+trainFile = os.path.join(root, "train.csv")
+
+print ("root directory: " + root)
+print ("file location (store): " + storeFile)
+print ("file location (train): " + trainFile)
+
