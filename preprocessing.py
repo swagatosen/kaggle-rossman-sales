cebf4fe8335a8f065fc53c57f78ac6344a6b6d99
Swagato S <swagato.sen@gmail.com>
2017-12-08 10:14:39 +0800

    completed new dimension processing table

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 
 def OneHotEncodeColumn(df, columnName):
 	#find unique categories in data
@@ -22,45 +22,55 @@ def OneHotEncodeColumn(df, columnName):
 	#print(df)
 	return df
 
+# def SplitDateIntoDayMonthYear(args):
+# 	if len(args) == 3:
+# 		df = args[0]
+# 		columnName = args[1]
+# 		dateFormat = args[2]
+
+# 	return SplitDateIntoDayMonthYear(df, columnName, dateFormat)
+
+def ReceiveArgsInArray(argLength):
+	def decorator(func):
+		def wrapper(data, *args):
+			if len(args) == argLength:
+				return func(data, *args)
+			else:
+				return print("The number of arguments accepted by this function does not match. Function accepts {0} arguments.".format(argLength))
+		return wrapper
+	return decorator
+
+@ReceiveArgsInArray(2)
 def SplitDateIntoDayMonthYear(df, columnName, dateFormat):
-	if (dateFormat.upper() == "YYYY/MM/DD" or dateFormat.upper() == "YYYY-MM-DD"):
-		d = dateFormat.split('/')
-		split = ''
+	if df is not None:
+		if (dateFormat.upper() == "YYYY/MM/DD" or dateFormat.upper() == "YYYY-MM-DD"):
+			d = dateFormat.split('/')
+			split = ''
 
-		if (len(d) == 3):
-			# date format is yyyy/mm/dd
-			split = '/'
-			#year = int(d[0])
-			#month = int(d[1])
-			#date = int(d[2])
-			#validFormat = true
-
-		else:
-			d = dateFormat.split('-')
 			if (len(d) == 3):
-				# date format is yyyy-mm-dd
-				split = '-'
-				#year = int(d[0])
-				#month = int(d[1])
-				#date = int(d[2])
-				#validFormat = true
+				# date format is yyyy/mm/dd
+				split = '/'
 
-		if (split != ''):
-			for i, row in df.T.iteritems():
-			# print(df.loc[0, columnName].split(split))
-				d = df.loc[0, columnName].split(split)
-				df.at[i, columnName + "_year"] = d[0]
-				df.at[i, columnName + "_month"] = d[1]
-				df.at[i, columnName + "_date"] = d[2]
-			#for i, row in df.T.iteritems():
+			else:
+				d = dateFormat.split('-')
+				if (len(d) == 3):
+					# date format is yyyy-mm-dd
+					split = '-'
 
-				#df.loc[i, columnName + "_year"] = 
-				#df[columnName + "_month"]
 
-	print(df)
+			if (split != ''):
+				for i, row in df.T.iteritems():
+				# print(df.loc[0, columnName].split(split))
+					d = df.loc[i, columnName].split(split)
+					df.at[i, columnName + "_year"] = d[0]
+					df.at[i, columnName + "_month"] = d[1]
+					df.at[i, columnName + "_date"] = d[2]
 
-def SplitDateIntoDayMonthYear2(row, columnName, dateFormat):
 
+		print(df)
+
+ReceiveArgsInArray(1)
+def SplitDateIntoDayMonthYear2(date, dateFormat):
 	try:
 		if (dateFormat.upper() == "YYYY/MM/DD" or dateFormat.upper() == "YYYY-MM-DD"):
 			d = dateFormat.split('/')
@@ -77,13 +87,13 @@ def SplitDateIntoDayMonthYear2(row, columnName, dateFormat):
 					split = '-'
 
 			if (split != ''):
-				d = row[columnName].split(split)
+				d = date.split(split)
 				assert len(d) == 3
 
 				# columns = pandas.Series({columnName + "_year": d[0], columnName + "_month": d[1], columnName + "_date": d[2] })
 				# print(columns)
 				# return columns
-				return d[0], d[1], d[2]
+				return [d[0], d[1], d[2]]
 
 	except AssertionError:
 		print("Test failed in SplitDateIntoDayMonthYear2. d: ")
@@ -99,3 +109,48 @@ def CreateEmptyMonthColumns(df, display="number"):
 		for num in range(1, 13):
 			df["month_" + num] = 0
 
+def CreateEmptyDayColumns(df):
+	if df is not None:
+		for num in range(1, 32):
+			df["month_" + num] = 0
+
+def ProcessDimensionDf(column_chunk, columnName, **kwargs):
+	if column_chunk is not None:
+		output = kwargs.get('output')
+		if output is None:
+			#create new output df
+			dfColumns = kwargs.get('outputColumns')
+			if (dfColumns is not None) and (len(dfColumns) > 0):
+				print('Creating new df with columns: ' + str(dfColumns))
+				output = CreateEmptyDf(columns=dfColumns)
+			else:
+				print('No output df is provided. Column names for new output is missing as well.')
+				return None
+
+		# process column of data now
+
+		func = kwargs.get('funcProcessColumn')
+		func_args = kwargs.get('funcProcessColumnArgs')
+		if func is not None:
+			for i, row in column_chunk.iteritems():
+				# try: 
+					data = row
+					# print(data)
+					if not output.index.contains(data):
+						#create new row in df with this index
+						output.loc[data] = func(data, func_args)
+				# except:
+				# 	print("column name: " + columnName + " does not exist in this df. Terminating processing.")
+				# 	return output
+		else:
+			print('No function found to process column data. No additional data has been added to output df.')
+		
+		print(output)
+		return output
+
+
+def CreateEmptyDf(**kwargs):
+	columns = kwargs.get('columns')
+	if columns is not None:
+		output = pd.DataFrame(columns=columns)
+		return output
---
+++ train2.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import preprocessing
+
+import os
+
+# data files available for training
+# train.csv, store.csv
+
+# locations of data files
+root = os.path.dirname(os.path.realpath(__file__))
+storeFileDir = os.path.join(root, "data", "store.csv")
+trainFileDir = os.path.join(root, "data", "train.csv")
+
+print ("root directory: " + root)
+print ("file location (store): " + storeFileDir)
+print ("file location (train): " + trainFileDir)
+
+dfStore = pd.read_csv(storeFileDir, dtype={'Store': str, 'StoreType': str, 'Assortment': str, 'PromoInterval': str})
+dfTrain = pd.read_csv(trainFileDir, dtype={'Store': str})
+
+# storeReader = pd.read_csv(storeFileDir, sep=',', iterator=True)
+# trainReader = pd.read_csv(trainFileDir, sep=',', iterator=True)
+# dfStore = storeReader.get_chunk(1000)
+# dfStore = trainReader.get_chunk(1000)
+
+storeColumns = list(dfStore.columns.values)
+trainColumns = list(dfTrain.columns.values)
+
+print("Store columns: " + str(storeColumns))
+print("Store sample data: \n\n")
+dfStore.info()
+print(dfStore.describe())
+print(dfStore.head(10))
+# print(storeSampleData)
+print("\n\nTrain columns: " + str(trainColumns))
+print("Train sample data: \n\n")
+# print(trainSampleData)
+dfTrain.info()
+print(dfTrain.describe())
+print(dfTrain.head(10))
+
+
+dfMerged = pd.merge(dfTrain, dfStore, how='left', left_on=['Store'], right_on=['Store'])
+print("\n\nMerged sample data: ")
+# print(mergedSampleData)
+print(dfMerged.info())
+print(dfMerged.describe())
+
+
+
+#split date into day, month and year
+dfMerged = dfMerged.iloc[:10000]
+dfMerged.info()
+# dfMerged['Date_year'] = ''
+# dfMerged['Date_month'] = ''
+# dfMerged['Date_date'] = ''
+# dfMerged[['Date_year', 'Date_month', 'Date_date']] = ''
+
+print(dfMerged)
+# dfMerged[['year', 'month', 'date']] = dfMerged['Date'].apply(lambda column: preprocessing.SplitDateIntoDayMonthYear2(column, 'Date', 'YYYY-MM-DD'), axis = 1)
+# dfMerged['Date'].apply(lambda row: preprocessing.SplitDateIntoDayMonthYear2(row, 'Date', 'YYYY-MM-DD'), axis = 1)
+# preprocessing.SplitDateIntoDayMonthYear(dfMerged, 'Date', 'YYYY-MM-DD')
+
+dfDateColumns = ['year', 'month', 'date']
+print(dfMerged['Date'])
+
+import timeit
+
+timeit.timeit(preprocessing.ProcessDimensionDf(dfMerged['Date'], 'Date', outputColumns=dfDateColumns, 
+			funcProcessColumn=preprocessing.SplitDateIntoDayMonthYear2, funcProcessColumnArgs=('YYYY/MM/DD')))
+
